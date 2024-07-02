305934eb406092964f72297cfc80c644ac8ec0e7
vishal <sarasani.vishalreddy@egovernments.org>
2024-07-02 15:45:50 +0530

    fixing duplicate individual issue

--- apps/health_campaign_field_worker_app/lib/blocs/beneficiary_registration/beneficiary_registration.dart
+++ apps/health_campaign_field_worker_app/lib/blocs/beneficiary_registration/beneficiary_registration.dart
@@ -407,6 +407,9 @@ class BeneficiaryRegistrationBloc
       addMember: (value) async {
         emit(value.copyWith(loading: true));
         try {
+          final createdAt = DateTime.now().millisecondsSinceEpoch;
+          final initialModifiedAt = DateTime.now().millisecondsSinceEpoch;
+
           await individualRepository.create(
             event.individualModel.copyWith(
               address: [
@@ -414,13 +417,22 @@ class BeneficiaryRegistrationBloc
                   id: null,
                   relatedClientReferenceId:
                       event.individualModel.clientReferenceId,
+                  auditDetails: AuditDetails(
+                    createdBy: event.userUuid,
+                    createdTime: createdAt,
+                    lastModifiedTime: createdAt,
+                    lastModifiedBy: event.userUuid,
+                  ),
+                  clientAuditDetails: ClientAuditDetails(
+                    createdTime: createdAt,
+                    lastModifiedTime: initialModifiedAt,
+                    lastModifiedBy: event.userUuid,
+                    createdBy: event.userUuid,
+                  ),
                 ),
               ],
             ),
           );
-
-          final createdAt = DateTime.now().millisecondsSinceEpoch;
-          final initialModifiedAt = DateTime.now().millisecondsSinceEpoch;
 
           var cycleIndex = "";
           if (value.householdModel.additionalFields != null) {
