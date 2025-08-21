4e9dde684611df39d89f9b0527bf27eff2895656
aviral-beehyv <aviral.gupta@beehyv.com>
2025-08-21 13:25:43 +0530

    fixed facility filtering shown on the facility selection page while recording stock transactions based on the transaction type

--- apps/health_campaign_field_worker_app/lib/pages/inventory/record_stock/warehouse_details.dart
+++ apps/health_campaign_field_worker_app/lib/pages/inventory/record_stock/warehouse_details.dart
@@ -108,20 +108,57 @@ class _WarehouseDetailsPageState extends LocalizedState<WarehouseDetailsPage> {
             );
           },
           builder: (ctx, facilityState) {
-            final facilities = facilityState.whenOrNull(
-                  fetched: (facilities, _, __) {
+            final allFacilities = facilityState.whenOrNull(
+                  fetched: (facilities, allFacilities, __) {
                     List<FacilityModel> teamFacilities = [];
                     teamFacilities.addAll(
-                      facilities,
+                      allFacilities,
                     );
 
                     return isDistributor && !isWareHouseMgr
                         ? teamFacilities
-                        : facilities;
+                        : allFacilities;
                   },
                 ) ??
                 [];
-            // get distribution facilities for communityDistributor , solution customisation
+
+            // filter facility logic based on boundary type
+            List<FacilityModel> facilities = allFacilities;
+            List<FacilityModel> filteredFacilityResult = [];
+            if (ctx.selectedProject.address?.boundaryType == 'Country') {
+              filteredFacilityResult = allFacilities
+                  .where((element) => element.usage == 'Central Facility')
+                  .toList();
+              facilities = filteredFacilityResult.isEmpty
+                  ? facilities
+                  : filteredFacilityResult;
+            } else if (ctx.selectedProject.address?.boundaryType == 'DRS') {
+              filteredFacilityResult = allFacilities
+                  .where((element) => element.usage == 'DRS Facility')
+                  .toList();
+              facilities = filteredFacilityResult.isEmpty
+                  ? facilities
+                  : filteredFacilityResult;
+            } else if (ctx.selectedProject.address?.boundaryType ==
+                'District') {
+              filteredFacilityResult = allFacilities
+                  .where((element) => element.usage == 'District Facility')
+                  .toList();
+              facilities = filteredFacilityResult.isEmpty
+                  ? facilities
+                  : filteredFacilityResult;
+            } else if (ctx.selectedProject.address?.boundaryType ==
+                'Poste De Sante') {
+              filteredFacilityResult = allFacilities
+                  .where(
+                    (element) => element.usage == 'Poste De Sante Facility',
+                  )
+                  .toList();
+              facilities = filteredFacilityResult.isEmpty
+                  ? facilities
+                  : filteredFacilityResult;
+            }
+
             List<FacilityModel> filteredFacilities = [];
             if (isSupervisor) {
               filteredFacilities = facilities
