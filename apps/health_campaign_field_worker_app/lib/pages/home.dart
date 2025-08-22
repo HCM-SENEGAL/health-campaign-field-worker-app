d93464ca8ab27dfdb3389a65f2f14af38e4b4686
aviral-beehyv <aviral.gupta@beehyv.com>
2025-08-22 20:40:09 +0530

    changed package name

--- apps/health_campaign_field_worker_app/lib/pages/home.dart
+++ apps/health_campaign_field_worker_app/lib/pages/home.dart
@@ -498,12 +498,10 @@ class _HomePageState extends LocalizedState<HomePage> {
     ];
 
     final List<String> filteredLabels = homeItemsLabel
-        .where((element) =>
-            state.actionsWrapper.actions
-                .map((e) => e.displayName)
-                .toList()
-                .contains(element) ||
-            (element == i18.home.db))
+        .where((element) => state.actionsWrapper.actions
+            .map((e) => e.displayName)
+            .toList()
+            .contains(element))
         .toList();
 
     final showcaseKeys =
